cfc052edfc4a6ed156947a6346b8a74556d37383
Giulia Sousa <giuliamariatech@gmail.com>
2026-03-06 23:40:37 -0300

    generate the angular project and dependencies

---
+++ shopping-list/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ListaComprasComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'shopping-list';
+}
