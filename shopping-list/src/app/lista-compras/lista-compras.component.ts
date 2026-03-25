01a24eaa09aac1514d86fd1f4584db153e4e05ed
Giulia Sousa <giuliamariatech@gmail.com>
2026-03-24 22:42:45 -0300

    feat: implement item list functionality with form handling

--- shopping-list/src/app/lista-compras/lista-compras.component.ts
+++ shopping-list/src/app/lista-compras/lista-compras.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './item-lista-interface';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
+  item: string = '';
+  listaDeItens: ItemLista[] = [];
 
+  adicionarItemLista() {
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.listaDeItens.length + 1;
+
+    this.listaDeItens.push(itemLista);
+    this.item = '';
+  }
 }
